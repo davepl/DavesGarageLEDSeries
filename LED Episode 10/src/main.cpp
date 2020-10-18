3bd5ff2c71c86b21dcbe2af32f3717956fc638f0
Dave Plummer <davepl@davepl.com>
2020-10-18 11:02:43 -0700

    FInished Episode 09

---
+++ LED Episode 10/src/main.cpp
@@ -0,0 +1,152 @@
+//+--------------------------------------------------------------------------
+//
+// NightDriver - (c) 2018 Dave Plummer.  All Rights Reserved.
+//
+// File:        LED Episode 08
+//
+// Description:
+//
+//   Draws sample effects on a an addressable strip using FastLED
+//
+// History:     Sep-15-2020     davepl      Created
+//              Oct-05-2020     davepl      Revised for Episode 07
+//              Oct-11-2020     davepl      Revised for Episode 08
+//              Oct-16-2020     davepl      Revised for Episode 09
+//---------------------------------------------------------------------------
+
+#include <Arduino.h>            // Arduino Framework
+#include <U8g2lib.h>            // For text on the little on-chip OLED
+#define FASTLED_INTERNAL        // Suppress build banner
+#include <FastLED.h>
+
+#define OLED_CLOCK  15          // Pins for the OLED display
+#define OLED_DATA   4
+#define OLED_RESET  16
+
+#define NUM_LEDS    40          // FastLED definitions
+#define LED_PIN     5
+
+CRGB g_LEDs[NUM_LEDS] = {0};    // Frame buffer for FastLED
+
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C g_OLED(U8G2_R2, OLED_RESET, OLED_CLOCK, OLED_DATA);
+int g_lineHeight = 0;
+int g_Brightness = 255;         // 0-255 LED brightness scale
+int g_PowerLimit = 900;         // 900mW Power Limit
+
+#define ARRAYSIZE(x) (sizeof(x)/sizeof(x[0]))
+#define TIMES_PER_SECOND(x) EVERY_N_MILLISECONDS(1000/x)
+
+// FractionalColor
+//
+// Returns a fraction of a color; abstracts the fadeToBlack out to this function in case we
+// want to improve the color math or do color correction all in one location at a later date.
+
+CRGB ColorFraction(CRGB colorIn, float fraction)
+{
+  fraction = min(1.0f, fraction);
+  return CRGB(colorIn).fadeToBlackBy(255 * (1.0f - fraction));
+}
+
+void DrawPixels(float fPos, float count, CRGB color)
+{
+  // Calculate how much the first pixel will hold
+  float availFirstPixel = 1.0f - (fPos - (long)(fPos));
+  float amtFirstPixel = min(availFirstPixel, count);
+  float remaining = min(count, FastLED.size()-fPos);
+  int iPos = fPos;
+
+  // Blend (add) in the color of the first partial pixel
+
+  if (remaining > 0.0f)
+  {
+    FastLED.leds()[iPos++] += ColorFraction(color, amtFirstPixel);
+    remaining -= amtFirstPixel;
+  }
+
+  // Now draw any full pixels in the middle
+
+  while (remaining > 1.0f)
+  {
+    FastLED.leds()[iPos++] += color;
+    remaining--;
+  }
+
+  // Draw tail pixel, up to a single full pixel
+
+  if (remaining > 0.0f)
+  {
+    FastLED.leds()[iPos] += ColorFraction(color, remaining);
+  }
+}
+
+void DrawMarqueeComparison()
+{
+  static float scroll = 0.0f;
+  scroll += 0.1f;
+  if (scroll > 5.0)
+    scroll -= 5.0;
+
+  for (float i = scroll; i < NUM_LEDS/2 -1; i+= 5)
+  {
+    DrawPixels(i, 3, CRGB::Green);
+    DrawPixels(NUM_LEDS-1-(int)i, 3, CRGB::Red);
+  }
+}
+
+
+void setup() 
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+
+  Serial.begin(115200);
+  while (!Serial) { }
+  Serial.println("ESP32 Startup");
+
+  g_OLED.begin();
+  g_OLED.clear();
+  g_OLED.setFont(u8g2_font_profont15_tf);
+  g_lineHeight = g_OLED.getFontAscent() - g_OLED.getFontDescent();        // Descent is a negative number so we add it to the total
+
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(g_LEDs, NUM_LEDS);               // Add our LED strip to the FastLED library
+  FastLED.setBrightness(g_Brightness);
+  set_max_power_indicator_LED(LED_BUILTIN);                               // Light the builtin LED if we power throttle
+  FastLED.setMaxPowerInMilliWatts(g_PowerLimit);                          // Set the power limit, above which brightness will be throttled
+}
+
+void loop() 
+{
+  bool bLED = 0;
+
+  while (true)
+  {
+    EVERY_N_MILLISECONDS(20)
+    {
+      /*
+      fadeToBlackBy(g_LEDs, NUM_LEDS, 64);
+      int cometSize = 5;
+      int iPos = beatsin16(32, 0, NUM_LEDS-cometSize);
+      byte hue = beatsin8(96);
+      for (int i = iPos; i < iPos + cometSize; i++)
+        g_LEDs[i] = CHSV(hue, 255, 255);
+      */
+
+      DrawMarqueeComparison();
+    }
+
+    EVERY_N_MILLISECONDS(250)
+    {
+      g_OLED.clearBuffer();
+      g_OLED.setCursor(0, g_lineHeight);
+      g_OLED.printf("FPS  : %u", FastLED.getFPS());
+      g_OLED.setCursor(0, g_lineHeight * 2);
+      g_OLED.printf("Power: %u mW", calculate_unscaled_power_mW(g_LEDs, NUM_LEDS));
+      g_OLED.setCursor(0, g_lineHeight * 3);
+      g_OLED.printf("Brite: %d", calculate_max_brightness_for_power_mW(g_Brightness, g_PowerLimit));
+      g_OLED.sendBuffer();
+    }
+
+    FastLED.setBrightness(g_Brightness);        //  Set the brightness scale
+    FastLED.delay(10);                          //  Show and delay
+  }
+}
